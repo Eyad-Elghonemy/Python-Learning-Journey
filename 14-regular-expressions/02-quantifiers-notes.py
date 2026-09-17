af93149916ad852d36477d4b5ee7711a051e0764
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-17 14:32:59 +0300

    Add 02-quantifiers-notes.py

---
+++ 14-regular-expressions/02-quantifiers-notes.py
@@ -0,0 +1,12 @@
+# ----------------------------------
+# -- Regular Expressions => Intro --
+# ----------------------------------
+# 8 0 or more
+# + 1 or more
+# ? 0 or 1
+# {2} Excatly 2 
+# {2, 5} Between 2 and 5
+# {2,} 2 or more
+# {,5} Up to 5
+# ------------
+# https://regex101.com/
