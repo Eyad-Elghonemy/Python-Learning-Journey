e052e5be3a6184c4bac5d48b99f5dd40717d781f
Eyad-Elghonemy <eyad0758@gmail.com>
2026-09-17 14:32:34 +0300

    Add /14-regular-expressions

---
+++ 14-regular-expressions/01-regex-introduction-notes.py
@@ -0,0 +1,42 @@
+# ----------------------------------
+# -- Regular Expressions => Intro --
+# ----------------------------------
+# [1] Sequence Of Characters That Define A Search Pattern
+# [2] Regualr Experssion Is Not In Python Its Genral Concept
+# [3] Used In [Credit Card Validation, IP Address Validation, Email Validation]
+# [4] Test RegEx "https://pythex.org/"
+# [5] Characters Sheet "https://WWW.debuggex.com/cheatsheet/regex/python"
+# -----------------------------------------------------------
+
+
+# [ab-d]	One character of: a, b, c, d
+
+# [^ab-d]	One character except: a, b, c, d
+
+# [\b]	Backspace character
+
+# \d	One digit
+
+# \D	One non-digit
+
+# \s	One whitespace
+
+# \S	One non-whitespace
+
+# \w	One word character
+
+# \W	One non-word character
+
+################################################
+
+# .	Any character except newline
+
+# a	The character a
+
+# ab	The string ab
+
+# a|b	a or b
+
+# a*	0 or more a's
+
+# \	Escapes a special character
